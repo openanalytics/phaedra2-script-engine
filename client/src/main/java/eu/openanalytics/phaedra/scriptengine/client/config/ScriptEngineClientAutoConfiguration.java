ee4354de7e659f77226a45375069595bb2498a58
Tobia De Koninck <tobia.dekoninck@openanalytics.eu>
2021-09-07 10:42:28 +0200

    Make it possible to disable client

--- client/src/main/java/eu/openanalytics/phaedra/scriptengine/client/config/ScriptEngineClientAutoConfiguration.java
+++ client/src/main/java/eu/openanalytics/phaedra/scriptengine/client/config/ScriptEngineClientAutoConfiguration.java
@@ -7,12 +7,14 @@ import org.springframework.amqp.core.Queue;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.amqp.rabbit.listener.DirectMessageListenerContainer;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import java.util.Map;
 
 @Configuration
+@ConditionalOnProperty(value = "phaedra2.scriptengine.client.enabled", havingValue = "true", matchIfMissing = true)
 public class ScriptEngineClientAutoConfiguration {
 
     private final ScriptEngineClientConfiguration clientConfig;
